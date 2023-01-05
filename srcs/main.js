7f8077af977de21e2cad337a3ec21dc7f47606c0
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-01-05 12:19:38 +0000

    added a simple canvas

--- srcs/main.js
+++ srcs/main.js
@@ -1,1 +1,16 @@
-console.log("Cube3D");
+const resizeCanvas = function ()
+{
+	const canvas = document.querySelector("#canvas");
+	canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth;
+}
+
+window.addEventListener("load", () =>{
+	const canvas = document.querySelector("#canvas");
+	const contex = canvas.getContext("2d");
+
+	//Resizing canvas - will have the size of browser window
+	resizeCanvas();
+});
+
+window.addEventListener("resize", resizeCanvas);
