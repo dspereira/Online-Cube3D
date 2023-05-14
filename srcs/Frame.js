1bf6f7416597abf0f1dbdd3441a32c67e8283126
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-05-14 19:26:12 +0100

    Updated Frame class

--- srcs/Frame.js
+++ srcs/Frame.js
@@ -1,3 +1,39 @@
+function RgbaHexFactory(hex)
+{
+	return {
+		getRed(){
+			return ((hex >> 24) & 0xFF);
+		},
+		getGreen(){
+			return ((hex >> 16) & 0xFF);
+		},
+		getBlue(){
+			return ((hex >> 8) & 0xFF);
+		},
+		getAlpha(){
+			return (hex & 0xFF);
+		}	
+	}
+}
+
+function RgbaFactory(red, green, blue, alpha)
+{
+	return {
+		getRed(){
+			return (red);
+		},
+		getGreen(){
+			return (green);
+		},
+		getBlue(){
+			return (blue);
+		},
+		getAlpha(){
+			return (alpha);
+		}	
+	}
+}
+
 class Frame {
 	#frame;
 	constructor(frame) 
@@ -11,29 +47,31 @@ class Frame {
 	}
 	getPixelColor(x, y)
 	{
-		/*const bpp = 4;
+		const bpp = 4;
 		const lineLen = bpp * this.frame.width;
 		const pos = x * bpp + y * lineLen;
 
-		return (RGBAFactory_1(
-			this.frame.data[pos], 
-			this.frame.data[pos + 1], 
-			this.frame.data[pos + 2], 
-			this.frame.data[pos + 3])
-		);*/
+		return (RgbaFactory(
+				this.frame.data[pos], 
+				this.frame.data[pos + 1], 
+				this.frame.data[pos + 2], 
+				this.frame.data[pos + 3]
+			)
+		);
 	}
 
-	setPixelColor(x, y, colorRgbaObj)
+	setPixelColor(x, y, colorRgbaHex)
 	{
 		// verificar se x é superior a weith
 		// verificar se x é superior a height
 		const bpp = 4;
 		const lineLen = bpp * this.frame.width;
 		const pos = x * bpp + y * lineLen;
+		const colorObj = RgbaHexFactory(colorRgbaHex);
 
-		this.frame.data[pos] = colorRgbaObj.getRed();
-		this.frame.data[pos + 1] = colorRgbaObj.getGreen();
-		this.frame.data[pos + 2] = colorRgbaObj.getBlue();
-		this.frame.data[pos + 3] = colorRgbaObj.getAlpha();
+		this.frame.data[pos] = colorObj.getRed();
+		this.frame.data[pos + 1] = colorObj.getGreen();
+		this.frame.data[pos + 2] = colorObj.getBlue();
+		this.frame.data[pos + 3] = colorObj.getAlpha();
 	}
 }
--- srcs/newmain.js
+++ srcs/newmain.js
@@ -1,49 +1,21 @@
-function RGBAFactory(hex)
-{
-	return {
-		getRed(){
-			return ((hex >> 24) & 0xFF);
-		},
-		getGreen(){
-			return ((hex >> 16) & 0xFF);
-		},
-		getBlue(){
-			return ((hex >> 8) & 0xFF);
-		},
-		getAlpha(){
-			return (hex & 0xFF);
-		}	
-	}
-}
-
-/*
-function RGBAFactory_1(red, green, blue, alpha)
-{
-	return {
-		getRed(){
-			return (red);
-		},
-		getGreen(){
-			return (green);
-		},
-		getBlue(){
-			return (blue);
-		},
-		getAlpha(){
-			return (alpha);
-		}	
-	}
-}
-*/
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const frame = new Frame(ctx.createImageData(1000, 1000));
 
+
 for (let i = 0; i < 1000; i++){
 	for (let j = 0; j < 1000; j++){
-		frame.setPixelColor(j,i,RGBAFactory(0xff0000ff));
+		frame.setPixelColor(j, i, 0xff0000ff);
 	}
 }
+
+const obj = frame.getPixelColor(100, 100);
+
+console.log(obj.getRed());
+console.log(obj.getBlue());
+console.log(obj.getGreen());
+console.log(obj.getAlpha());
+
 ctx.putImageData(frame.getFrame(), 0, 0);
